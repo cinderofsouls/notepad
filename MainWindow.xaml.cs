7c1f96c7f77ddee4eec7ce022116c35c63b7c32b
cinderofsouls <trevor.tilby@gmail.com>
2021-06-12 19:52:15 -0600

    more menu bar

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -15,14 +15,43 @@ using System.Windows.Shapes;
 
 namespace notepad
 {
+    public class ExitKey : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            //MessageBox.Show("Ctrl+Shift+X");
+            Application.Current.Shutdown();
+        }
+    }
+
+    public class ExitCommandContext
+    {
+        public ICommand ExitCommand
+        {
+            get
+            {
+                return new ExitKey();
+            }
+        }
+    }
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = new ExitCommandContext();
         }
 
         private void MenuItemExit_Click(object sender, RoutedEventArgs e)
@@ -38,6 +67,26 @@ namespace notepad
         private void CommandBindingNew_Executed(object sender, ExecutedRoutedEventArgs e)
         {
             MessageBox.Show("New MenuItem Clicked!");
+        }
+
+        private void CommandBindingOpen_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void CommandBindingOpen_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("Open MenuItem Clicked!");
+        }
+
+        private void CommandBindingSave_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void CommandBindingSave_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("Save MenuItem Clicked!");
         }
     }
 }

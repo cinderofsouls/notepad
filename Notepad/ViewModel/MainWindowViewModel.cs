030ca3fd3f75df8f077dd12d7f65585b463cd1f5
cinderofsouls <trevor.tilby@gmail.com>
2021-08-13 23:18:53 -0600

    created initial viewmodel

---
+++ Notepad/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
+
+namespace Notepad
+{
+    class MainWindowViewModel : ViewModelBase
+    {
+        private string mainText = "test default text";
+
+        public string MainText
+        {
+            get { return mainText; }
+            set {
+                mainText = value;
+                RaisePropertyChanged();
+            }
+        }
+
+    }
+}

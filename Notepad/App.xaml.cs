f4d9df0e03c202e34251a7e888bda4be42faad41
cinderofsouls <trevor.tilby@gmail.com>
2021-08-15 18:53:14 -0600

    put startupeventargs into an application resource

--- Notepad/App.xaml.cs
+++ Notepad/App.xaml.cs
@@ -13,5 +13,11 @@ namespace Notepad
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            MainWindow wnd = new MainWindow();
+            Application.Current.Resources.Add("CommandLineArguments", e);
+            wnd.Show();
+        }
     }
 }
